2813d4a8e73c81f5d7b8a7448ff6748c34937046
yerassyl.maikhanov <yerassyl.maikhanov@uco.kz>
2020-03-22 15:54:55 +0600

    fix bug

--- lib/service/utils.dart
+++ lib/service/utils.dart
@@ -6,6 +6,6 @@ class Utils {
   /// creating REST url : http://localhost:8080/test/v2/services/serviceName/methodName
   static String createRestUrl(String serviceName, String methodName, Types type) {
     String urlSuffix = UrlTypes.path[type];
-    return "${GlobalVariables.urlEndPoint}/v2/$urlSuffix/$serviceName/$methodName";
+    return "${GlobalVariables.urlEndPoint}/rest/v2/$urlSuffix/$serviceName/$methodName";
   }
 }
